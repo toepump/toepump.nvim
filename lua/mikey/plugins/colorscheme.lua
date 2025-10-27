e1f31b9d18c3e467f35faeb6fe15484b1cbfc0d2
toepump <mikeykentarocho@gmail.com>
2025-10-27 19:57:30 +0900

    colorscheme and nvim-colorizer

--- lua/mikey/plugins/colorscheme.lua
+++ lua/mikey/plugins/colorscheme.lua
@@ -11,6 +11,34 @@ return {
         -- Load the colorscheme here.
         -- Like many other themes, this one has different styles, and you could load
         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'catppuccin-frappe'
+        vim.cmd.colorscheme 'catppuccin-mocha'
     end,
 }
+
+-- return {
+--     -- You can easily change to a different colorscheme.
+--     -- Change the name of the colorscheme plugin below, and then
+--     -- change the command in the config to whatever the name of that colorscheme is.
+--     --
+--     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+--     'olimorris/onedarkpro.nvim',
+--     name = 'onedarkpro',
+--     priority = 1000, -- Make sure to load this before all the other start plugins.
+--     config = function()
+--         -- Load the colorscheme here.
+--         -- Like many other themes, this one has different styles, and you could load
+--         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--         vim.cmd.colorscheme 'onedark'
+--     end,
+-- }
+
+-- return {
+--     'navarasu/onedark.nvim',
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--         require('onedark').setup {
+--             style = 'deep',
+--         }
+--         vim.cmd.colorscheme 'onedark'
+--     end,
+-- }
