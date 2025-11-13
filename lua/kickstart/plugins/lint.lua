8bfb9734dfd4741e353db4b8b3820ac33688f909
toepump <mikeykentarocho@gmail.com>
2025-11-13 16:05:01 +0900

    use eslint for tsx and jsx
    - this ensures that the eslintrc rules and plugins are checked
    - it was previously working that ts_ls (lsp) provided some things, but
    that didn't include eslint plugins since eslint wasn't being used

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,7 +9,9 @@ return {
                 -- markdown = { 'markdownlint' },
                 gdscript = { 'gdlint' },
                 typescript = { 'eslint_d' },
+                typescriptreact = { 'eslint_d' },
                 javascript = { 'eslint_d' },
+                javascriptreact = { 'eslint_d' },
             }
 
             -- To allow other plugins to add linters to require('lint').linters_by_ft,
